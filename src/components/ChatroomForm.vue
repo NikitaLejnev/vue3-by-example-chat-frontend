37d10185791b417743680ae7e42d685ad162a94a
NikitaLejnev <catassasin331@gmail.com>
2022-01-30 10:44:27 +0300

    Added <template> to ChatroomForm.vue

---
+++ src/components/ChatroomForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      {{
+        edit ? "Edit" : "Add
+      }} Chatroom
+    </h1>
+    <form @submit.prevent="submit">
+      <div class="form-field">
+        <label for="name">Name</label>
+        <br />
+        <input v-model="form.name" type="text" name="name" />
+      </div>
+      <div>
+        <input type="submit" />
+      </div>
+    </form>
+  </div>
+</template>
