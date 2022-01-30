fe8407e4ec67da24885e023af3fe1771ff63c633
NikitaLejnev <catassasin331@gmail.com>
2022-01-30 10:52:44 +0300

    Finished ChatroomForm.vue

--- src/components/ChatroomForm.vue
+++ src/components/ChatroomForm.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <h1>
-      {{
-        edit ? "Edit" : "Add"
-      }} Chatroom
-    </h1>
+    <h1>{{ edit ? "Edit" : "Add" }} Chatroom</h1>
     <form @submit.prevent="submit">
       <div class="form-field">
         <label for="name">Name</label>
@@ -21,10 +17,45 @@
 <script>
 import axios from "axios";
 import { APIURL } from "../constants";
+
 export default {
   name: "ChatroomForm",
-
-  async created() {
+  props: {
+    edit: {
+      type: Boolean,
+      default: false,
+    },
+    id: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+      },
+    };
+  },
+  methods: {
+    async submit() {
+      const { name } = this.form;
+      if (!name) {
+        alert("Name is required");
+        return;
+      }
+      if (this.edit) {
+        await axios.put(`${APIURL}/api/chat/update/${this.id}`, {
+          name,
+        });
+      } else {
+        await axios.post(`${APIURL}/api/chat/create`, {
+          name,
+        });
+      }
+      this.$router.push("/");
+    },
+  },
+  async beforeMount() {
     if (this.edit) {
       const { data } = await axios.get(`${APIURL}/api/chat/${this.id}`);
       this.form = data;
